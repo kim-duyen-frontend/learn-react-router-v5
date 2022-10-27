892c73721c08b6ac9ad6465f369a81363eb54080
kim-duyen-frontend <ntduyen255@gmail.com>
2022-10-27 14:56:24 +0700

    design form login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import { useState } from 'react';
+import "./styles.scss";
 
-function Login() {
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: "eve.holt@reqres.in",
+        password: "cityslicka"
+    });
+    
+    const handleChangeField = (event, type) => {
+        const value = event.target.value;
+        formData[type] = value;
+        setFormData({ ...formData })
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log({ ...formData });
+    }
+    
     return (
-        <div>
-            <h1>Page Login</h1>
+        <div className="login">
+            <div className="container">
+                <div className="form-group">
+                    <form onSubmit={handleSubmit}>
+                        <label className="labelName">Email: </label>
+                        <input
+                            className="inputName"
+                            value={formData.email}
+                            placeholder="enter email..."
+                            type="email"
+                            onChange={(event) => handleChangeField(event, "email")}
+                        />
+                        <label className="labelPassword">Password: </label>
+                        <input
+                            className="inputPassword"
+                            value={formData.password}
+                            placeholder="enter password..."
+                            type="password"
+                            onChange={(event) => handleChangeField(event, "password")}
+                        />
+                        <button className="btnLogin">log in</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
